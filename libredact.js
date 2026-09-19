b5fbddc213f85bb1e85230811805f3d3a393c15d
kise82 <kise82@pm.me>
2026-09-19 20:18:25 +0000

    `Libredact`: Be more thorough on domain matching (on top of `@match`)
    Signed-off-by: kise82 <kise82@pm.me>

--- libredact.js
+++ libredact.js
@@ -12,19 +12,24 @@
 // ==/UserScript==
 
 // Front-end instances
-const X = 'xcancel.com';
-const REDDIT = 'redlib.catsarch.com';
+const MAPPING = {
+  'x.com': 'xcancel.com',
+  'reddit.com': 'redlib.catsarch.com',
+};
 
 (() => {
   'use strict';
 
-  const hostname = window.location.hostname;
-  let replacement = hostname;
-  if (hostname.endsWith('x.com')) {
-    replacement = X;
-  } else if (hostname.endsWith('reddit.com')) {
-    replacement = REDDIT;
-  }
+  let replacement = ((hostname) => {
+    for (const old of Object.keys(MAPPING)) {
+      if (hostname.endsWith(old) && (hostname.length == old.length || hostname.at(-(old.length + 1)) == '.')) {
+        return MAPPING[old];
+      }
+    }
+    return hostname;
+  })(window.location.hostname);
 
-  window.location.replace(window.location.href.replace(hostname, replacement));
+  window.location.replace(window.location.href.replace(window.location.hostname, replacement));
 })();
+
+
